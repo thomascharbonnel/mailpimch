229b059e06df0fb469446865b5a16513b6dfbcf1
Thomas Charbonnel <thomas@charbonnel.eu>
2017-06-30 14:17:47 +0800

    Impl ReadEmailAddresses

--- mailpimch.go
+++ mailpimch.go
@@ -1,13 +1,41 @@
 package main
 
-import "gopkg.in/gomail.v2"
+import (
+//  "gopkg.in/gomail.v2"
+  "bufio"
+  "os"
+)
 
-// Read the email addresses from a file
+// Helper function to take care of errors
+func handle_errors(err error) {
+  if err != nil {
+    panic(err)
+  }
+}
+
+// Read the email addresses from a file.
+// Each line should only contain one email address
 func ReadEmailAddresses(filename string) []string {
+  email_addresses := make([]string, 0)
+
+  // Open (and close) the file
+  file, err := os.Open(filename)
+  handle_errors(err)
+  defer file.Close()
+
+  // Read the file
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    email_addresses = append(email_addresses, scanner.Text())
+  }
+  handle_errors(scanner.Err())
+
+  return email_addresses
 }
 
 // Generate the message
 func GenerateMessage(email_address string) string {
+  return ""
 }
 
 // Send the emails
