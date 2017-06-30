dc0a2ae8bfd723e2f8fe4868dcd00d5236f0dc86
Thomas Charbonnel <thomas@charbonnel.eu>
2017-06-30 13:40:51 +0800

    Add basic structure

---
+++ mailpimch.go
@@ -0,0 +1,19 @@
+package main
+
+import "gopkg.in/gomail.v2"
+
+// Read the email addresses from a file
+func ReadEmailAddresses(filename string) []string {
+}
+
+// Generate the message
+func GenerateMessage(email_address string) string {
+}
+
+// Send the emails
+func SendEmails(email_addresses []string) {
+}
+
+// Parse CLI args and execute SendEmails
+func main() {
+}
